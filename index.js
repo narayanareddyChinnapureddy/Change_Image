ca0d44750a83f4fb3c80adda55ae9ee722e9dd2d
narayanareddy <narayanreddieui@gmail.com>
2021-10-14 11:18:02 +0530

    css changes done

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-let images = document.querySelectorAll('img');
-let image1 = document.getElementById('img1');
-let image2 = document.getElementById('img2');
-let image3 = document.getElementById('img3');
-let image4 = document.getElementById('img4');
-let image5 = document.getElementById('img5');
+let image = document.getElementById('img');
 
 let imgArray = [
 	'image1.jpg',
@@ -18,26 +13,7 @@ let imgArray = [
 ];
 
 var interval = setInterval(() => {
-	let random = Math.floor(Math.random() * 5);
-	image1.src = './img/' + imgArray[random];
-}, 2000);
-
-var interval = setInterval(() => {
-	let random = Math.floor(Math.random() * 6);
-	image2.src = './img/' + imgArray[random];
-}, 2000);
-
-var interval = setInterval(() => {
-	let random = Math.floor(Math.random() * 7);
-	image3.src = './img/' + imgArray[random];
-}, 2000);
-
-var interval = setInterval(() => {
-	let random = Math.floor(Math.random() * 8);
-	image4.src = './img/' + imgArray[random];
-}, 2000);
-
-var interval = setInterval(() => {
 	let random = Math.floor(Math.random() * 9);
-	image5.src = './img/' + imgArray[random];
+	image.src = './img/' + imgArray[random];
 }, 2000);
+
